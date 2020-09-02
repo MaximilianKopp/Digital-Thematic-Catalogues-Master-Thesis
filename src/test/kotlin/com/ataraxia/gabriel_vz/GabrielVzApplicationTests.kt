406b60310e6bf6e72751f92fa2c985f3e4761e8f
mkopp <m.kopp89@web.de>
2020-09-02 23:31:20 +0200

    Initial commit

---
+++ src/test/kotlin/com/ataraxia/gabriel_vz/GabrielVzApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ataraxia.gabriel_vz
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GabrielVzApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
