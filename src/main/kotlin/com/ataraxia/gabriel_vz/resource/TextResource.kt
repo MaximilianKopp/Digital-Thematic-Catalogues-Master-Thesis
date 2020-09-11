ebb8880f41b6ef9a861167df5899005e4423bea9
mkopp <m.kopp89@web.de>
2020-09-11 02:35:47 +0200

    Added swagger details to Texts and Works

--- src/main/kotlin/com/ataraxia/gabriel_vz/resource/TextResource.kt
+++ src/main/kotlin/com/ataraxia/gabriel_vz/resource/TextResource.kt
@@ -1,12 +1,42 @@
 package com.ataraxia.gabriel_vz.resource
 
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
 import org.springframework.hateoas.Link
 
+@ApiModel(value = "TextResource", description = "Represents a Text")
 class TextResource(
+        @ApiModelProperty(
+                notes = "A link to the specific Resource",
+                accessMode = ApiModelProperty.AccessMode.READ_ONLY
+        )
         var self: Link?,
+        @ApiModelProperty(
+                notes = "A globally unique identifier",
+                accessMode = ApiModelProperty.AccessMode.READ_ONLY,
+                example = "4028e3817478b2c7017478b2d0250000"
+        )
         var id: String?,
+        @ApiModelProperty(
+                notes = "The title of a Text",
+                accessMode = ApiModelProperty.AccessMode.READ_ONLY,
+                dataType = "String",
+                example = "Nine little Cats"
+        )
         var title: String,
+        @ApiModelProperty(
+                notes = "The author of this text",
+                accessMode = ApiModelProperty.AccessMode.READ_ONLY,
+                dataType = "String",
+                example = "Christoph van Cat"
+        )
         var author: String,
+        @ApiModelProperty(
+                notes = "An excerpt of the text",
+                accessMode = ApiModelProperty.AccessMode.READ_ONLY,
+                dataType = "String",
+                example = "Nine little cats are tiny little brags"
+        )
         var excerpt: String
 ) {
 
--- src/main/kotlin/com/ataraxia/gabriel_vz/resource/WorkResource.kt
+++ src/main/kotlin/com/ataraxia/gabriel_vz/resource/WorkResource.kt
@@ -1,27 +1,125 @@
 package com.ataraxia.gabriel_vz.resource
 
-import com.ataraxia.gabriel_vz.model.*
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
+import io.swagger.annotations.ApiModelProperty.AccessMode.READ_ONLY
 import org.springframework.hateoas.Link
 import org.springframework.hateoas.RepresentationModel
 
+@ApiModel(value = "WorkResource", description = "Represents a Work")
 class WorkResource(
+
+        @ApiModelProperty(
+                notes = "A link to the specific Resource",
+                accessMode = READ_ONLY
+        )
         var self: Link?,
+        @ApiModelProperty(
+                notes = "A globally unique identifier",
+                accessMode = READ_ONLY,
+                example = "4028e3817478b2c7017478b2d0250000"
+        )
         var id: String?,
+        @ApiModelProperty(
+                notes = "The title of a Work",
+                accessMode = READ_ONLY,
+                dataType = "String",
+                example = "Sonata in C-Dur"
+        )
         var title: String,
+        @ApiModelProperty(
+                notes = "Date when the Work was created",
+                accessMode = READ_ONLY,
+                dataType = "String",
+                example = "Sonata in C-Dur"
+        )
         var dateOfCreation: String,
+        @ApiModelProperty(
+                notes = "Date of the first performance",
+                accessMode = READ_ONLY,
+                dataType = "String",
+                example = "2015-09-21"
+        )
         var dateOfPremiere: String,
+        @ApiModelProperty(
+                notes = "Place of the first performance",
+                accessMode = READ_ONLY,
+                dataType = "String",
+                example = "2015-09-21"
+        )
         var placeOfPremiere: PlaceResource?,
+        @ApiModelProperty(
+                notes = "Further information about a Work",
+                accessMode = READ_ONLY,
+                dataType = "String",
+                example = "Gabriel wrote this work in his early period"
+        )
         var commentary: String,
+        @ApiModelProperty(
+                notes = "Mentions the person to whom this work is dedicated",
+                accessMode = READ_ONLY,
+                dataType = "String",
+                example = "For Mrs. Holly"
+        )
         var dedication: String,
+        @ApiModelProperty(
+                notes = "Describes the formation of instruments",
+                accessMode = READ_ONLY,
+                dataType = "String",
+                example = "Piano, Violin I, Violin II and Percussions"
+        )
         var instrumentation: String,
+        @ApiModelProperty(
+                notes = "Defines the genre of this piece",
+                accessMode = READ_ONLY,
+                dataType = "String",
+                example = "Piano music"
+        )
         var category: String,
+        @ApiModelProperty(
+                notes = "Defines the duration of a piece",
+                accessMode = READ_ONLY,
+                dataType = "String",
+                example = "33 Minutes"
+        )
         var duration: String,
+        @ApiModelProperty(
+                notes = "Name of the current editor",
+                accessMode = READ_ONLY,
+                dataType = "String",
+                example = "Luisa"
+        )
         var editor: String,
+        @ApiModelProperty(
+                notes = "Text template of a vocal piece",
+                accessMode = READ_ONLY,
+                dataType = "String",
+                example = "The nine cats"
+        )
         var relatedText: TextResource?,
+        @ApiModelProperty(
+                notes = "Set of recorded pieces",
+                accessMode = READ_ONLY,
+                dataType = "String",
+                example = "Wolfgang Gabriel- Complete Piano Music"
+        )
         var discographies: MutableSet<DiscographyResource>,
+        @ApiModelProperty(
+                notes = "Involved persons",
+                accessMode = READ_ONLY,
+                dataType = "String",
+                example = "Hans Schauer"
+        )
         var relatedPersons: MutableSet<PersonResource>,
+        @ApiModelProperty(
+                notes = "Related literature and references of this piece",
+                accessMode = READ_ONLY,
+                dataType = "String",
+                example = "Susanne Hübner - Wolfgang Gabriel Leben und Werk"
+        )
         var literatureList: MutableSet<LiteratureResource>
 ) : RepresentationModel<WorkResource>() {
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is WorkResource) return false
