5afbf6493136d00375de1f461443d34d176fe0ad
mkopp <m.kopp89@web.de>
2020-11-05 23:45:54 +0100

    Clean EditorWorkController

--- src/main/kotlin/com/ataraxia/gabriel_vz/controller/editor/EditorWorkController.kt
+++ src/main/kotlin/com/ataraxia/gabriel_vz/controller/editor/EditorWorkController.kt
@@ -1,7 +1,6 @@
 package com.ataraxia.gabriel_vz.controller.editor
 
 import com.ataraxia.gabriel_vz.factory.PlaceFactory
-import com.ataraxia.gabriel_vz.factory.TextFactory
 import com.ataraxia.gabriel_vz.model.Work
 import com.ataraxia.gabriel_vz.service.PlaceService
 import com.ataraxia.gabriel_vz.service.TextService
@@ -20,14 +19,11 @@ class EditorWorkController(
         val workService: WorkService,
         val placeFactory: PlaceFactory,
         val placeService: PlaceService,
-        val textFactory: TextFactory,
         val textService: TextService
 ) {
 
     @GetMapping("/createWork")
     fun createWork(model: Model): String {
-        val placeList = placeService.getAll().toOption().orNull()
-        placeList?.forEach { println("Hier ist es" + it.id) }
         model.addAttribute("work", Work())
         model.addAttribute("places", placeService.getAll().toOption().orNull())
         model.addAttribute("texts", textService.getAll().toOption().orNull())
@@ -39,19 +35,12 @@ class EditorWorkController(
         if (bindingResult.hasErrors()) {
             print(bindingResult.allErrors)
         }
-        println(work?.title + " das ist die Work name")
-        println(work?.placeOfPremiere?.id + "das ist der Place title")
-        //val place = placeService.get(work?.placeOfPremiere?.id!!).toOption().orNull()
-        //placeFactory.entityFromModel(place!!).addWork(workFactory.entityFromModel(work))
-        //workRepository.save(workFactory.entityFromModel(work!!))
 
         work?.apply {
             if (placeOfPremiere?.id == "") {
-                print("hier wird die id null")
                 this.placeOfPremiere = null
             }
             if (relatedText?.id == "") {
-                print("hier wird die Text id null")
                 this.relatedText = null
             }
         }
